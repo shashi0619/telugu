53fcf5d7317608fe2741877e78f7809da115e2f9
shashi s <shashichintu68@gmail.com>
2025-07-03 13:14:33 +0530

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Enable build to work on Netlify and Vercel
+  output: 'standalone',
+
+  // Allow external images from your S3 bucket
+  images: {
+    domains: ['shashimern.s3.eu-north-1.amazonaws.com'],
+  },
+
+  // Allow builds to pass even with ESLint warnings
   eslint: {
-    // Warnings will not fail the build
     ignoreDuringBuilds: true,
   },
 };
