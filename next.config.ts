07ea9faedbdbbd06e57e810d6935983f4322e225
shashi s <shashichintu68@gmail.com>
2025-07-03 12:42:54 +0530

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Warnings will not fail the build
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useMemo } from "react";
-import Image from "next/image";
+
 import { AppSidebar } from "@/components/app-sidebar";
 import {
   Breadcrumb,
