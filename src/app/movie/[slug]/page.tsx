ebc998215fcf135c502c395b795e1eb2ef020f18
shashi s <shashichintu68@gmail.com>
2025-08-06 18:28:59 +0530

    button

--- src/app/movie/[slug]/page.tsx
+++ src/app/movie/[slug]/page.tsx
@@ -77,66 +77,76 @@ export default async function MovieDetailsPage({ params }: Props) {
   if (!movie) return notFound();
 
   return (
-    <div className="p-6 max-w-4xl mx-auto space-y-6">
-      <div className="bg-blue-50 rounded-md p-4">
-        <h1 className="text-3xl font-bold mb-4">{movie.title}</h1>
-        <SafeImage
-          src={movie.image}
-          alt={movie.title}
-          className="w-full h-96 object-cover rounded-md mb-6"
-        />
+    <>
+      <div className="mb-4 mx-4 mt-4">
+        <a
+          href="/"
+          className="inline-block px-2 py-2 bg-black text-white rounded-lg hover:bg-white hover:text-black border border-black transition"
+        >
+          ← Back to Home
+        </a>
       </div>
+      <div className="p-6 max-w-4xl mx-auto space-y-6">
+        <div className="bg-blue-50 rounded-md p-4">
+          <h1 className="text-3xl font-bold mb-4">{movie.title}</h1>
+          <SafeImage
+            src={movie.image}
+            alt={movie.title}
+            className="w-full h-96 object-cover rounded-md mb-6"
+          />
+        </div>
 
-      <div className="bg-green-50 rounded-md p-4">
-        <p className="text-md text-gray-500">
-          <strong>Release Date:</strong> {movie.year}
-        </p>
-        <p className="text-md text-gray-500">
-          <strong>Genre:</strong> {movie.genre}
-        </p>
-      </div>
+        <div className="bg-green-50 rounded-md p-4">
+          <p className="text-md text-gray-500">
+            <strong>Release Date:</strong> {movie.year}
+          </p>
+          <p className="text-md text-gray-500">
+            <strong>Genre:</strong> {movie.genre}
+          </p>
+        </div>
 
-      <div className="bg-yellow-50 rounded-md p-4">
-        <p className="text-md text-gray-500">
-          <strong>Cast:</strong> {movie.cast}
-        </p>
-      </div>
+        <div className="bg-yellow-50 rounded-md p-4">
+          <p className="text-md text-gray-500">
+            <strong>Cast:</strong> {movie.cast}
+          </p>
+        </div>
 
-      <div className="bg-purple-50 rounded-md p-4">
-        <p className="text-lg text-gray-700 mb-2">
-          <strong>Description:</strong> {movie.description}
-        </p>
-      </div>
+        <div className="bg-purple-50 rounded-md p-4">
+          <p className="text-lg text-gray-700 mb-2">
+            <strong>Description:</strong> {movie.description}
+          </p>
+        </div>
 
-      <div className="bg-pink-50 rounded-md p-4">
-        <p className="text-md text-gray-500">
-          <strong>Category:</strong> {movie.category}
-        </p>
-        <p className="text-md text-gray-500">
-          <strong>Tags:</strong> {movie.tags}
-        </p>
-      </div>
+        <div className="bg-pink-50 rounded-md p-4">
+          <p className="text-md text-gray-500">
+            <strong>Category:</strong> {movie.category}
+          </p>
+          <p className="text-md text-gray-500">
+            <strong>Tags:</strong> {movie.tags}
+          </p>
+        </div>
 
-      <div className="bg-orange-50 rounded-md p-4">
-        <p className="text-md text-gray-500">
-          <strong>Trailer:</strong>
-          {movie.trailerUrl ? (
-            <div className="mt-2 flex justify-center">
-              <iframe
-                width="75%"
-                height="315"
-                src={movie.trailerUrl.replace("watch?v=", "embed/")}
-                title={`${movie.title} Trailer`}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                className="rounded-lg"
-              ></iframe>
-            </div>
-          ) : (
-            <span> Not available</span>
-          )}
-        </p>
+        <div className="bg-orange-50 rounded-md p-4">
+          <p className="text-md text-gray-500">
+            <strong>Trailer:</strong>
+            {movie.trailerUrl ? (
+              <div className="mt-2 flex justify-center">
+                <iframe
+                  width="75%"
+                  height="315"
+                  src={movie.trailerUrl.replace("watch?v=", "embed/")}
+                  title={`${movie.title} Trailer`}
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="rounded-lg"
+                ></iframe>
+              </div>
+            ) : (
+              <span> Not available</span>
+            )}
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
